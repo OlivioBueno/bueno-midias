5e64badd22aec6d4428cfe2eed9c95deaf597cc6
OLIVIO BUENO <oliviocbueno.1999@gmail.com>
2026-03-12 12:26:13 -0300

    corrige erro de compilação na página dvsmai

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,3 +21,8 @@ export default function RootLayout({ children }) {
     </html>
   )
 }
+
+<link
+  href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800;900&display=swap"
+  rel="stylesheet"
+/>
